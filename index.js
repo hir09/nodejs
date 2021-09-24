63a15944bc3101c927dc92b37237e783391c215b
Hiren Bhatt <61093544+hir09@users.noreply.github.com>
2021-09-24 19:57:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Testing DevOps Node! JS Project Hello How are you ? welcome to devOps Project')
+  response.send('Hello Testing DevOps Node! JS Project Hello How are you ? welcome to devOps Project Node JS -platform AWS')
 }) 
 
 app.listen(app.get('port'), function() {
