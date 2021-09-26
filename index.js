159074934bed85bc90905b43566bf9a3c93c280b
Hiren Bhatt <61093544+hir09@users.noreply.github.com>
2021-09-26 13:08:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('many congratulations !!!  your Node Js CICD runs healthy!!!')
+  response.send('successfully deployed CICD with Jenkins-->Docker-->NodeJS')
 }) 
 
 app.listen(app.get('port'), function() {
